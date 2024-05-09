62efb4b283d6fb64952407bbb7d18af408a6be33
weiyi <weiyi.wang.au@gmail.com>
2024-05-09 17:15:08 +1000

    getCrimeBySuburb works, but package name need rename

--- fission_functions/crime/crime.py
+++ fission_functions/crime/crime.py
@@ -1,5 +1,5 @@
 from elasticsearch import Elasticsearch
-from flask import jsonify
+from flask import jsonify, request
 
 #return all crime
 def get_all_crime():
@@ -17,3 +17,23 @@ def get_all_crime():
     hits = result["hits"]["hits"]
     crimes = list(map(lambda h: h['_source'], hits))
     return jsonify(crimes)
+
+
+#get crime by suburb name
+def get_crime_by_suburb():
+    client = Elasticsearch(
+        'https://elasticsearch-master.elastic.svc.cluster.local:9200',
+        verify_certs=False,
+        basic_auth=('elastic', 'elastic')
+    )
+    suburb = request.headers.get("X-Fission-Params-Suburb")
+    query = {
+        "query": {
+            "match": {
+                "suburb_name": suburb
+            }
+        }
+    }
+    result = client.search(index="crime_test", body=query)
+    crime = result["hits"]["hits"][0]['_source']
+    return jsonify(crime)
