33765361378aa2bd2c541a4c2a5aba260f215852
董梦函 <dongmenghan@ravpn-200-student-10-8-1-71.uniaccess.unimelb.edu.au>
2024-05-19 15:44:29 +1000

    Change accoring to CI

--- fission_functions/http_handlers/handlers.py
+++ fission_functions/http_handlers/handlers.py
@@ -13,10 +13,10 @@ def get_all_suburbs():
 def get_crime_by_suburb():
     suburb_name = request.headers.get("X-Fission-Params-Suburb")
     suburb_info = SuburbService.get_suburb_info_by_name(suburb_name)
-    
+
     if not suburb_info:
         return make_response(jsonify({"error": "Suburb not found"}), 404)
-    
+
     lga_code = suburb_info["lga_code"]
     crime = CrimeService.get_crime_by_suburb(lga_code)
     return make_response(jsonify(crime), 200)
@@ -25,10 +25,10 @@ def get_crime_by_suburb():
 def get_weather_by_suburb():
     suburb_name = request.headers.get("X-Fission-Params-Suburb")
     suburb_info = SuburbService.get_suburb_info_by_name(suburb_name)
-    
+
     if not suburb_info:
         return make_response(jsonify({"error": "Suburb not found"}), 404)
-    
+
     lga_code = suburb_info["lga_code"]
     weather = WeatherService.get_weather_by_suburb(lga_code)
     return make_response(jsonify(weather), 200)
@@ -37,10 +37,10 @@ def get_weather_by_suburb():
 def get_suburb_crime_and_weather():
     suburb_name = request.headers.get("X-Fission-Params-Suburb")
     suburb_info = SuburbService.get_suburb_info_by_name(suburb_name)
-    
+
     if not suburb_info:
         return make_response(jsonify({"error": "Suburb not found"}), 404)
-    
+
     lga_code = suburb_info["lga_code"]
     crime = CrimeService.get_crime_by_suburb(lga_code)
     weather = WeatherService.get_weather_by_suburb(lga_code)
@@ -50,14 +50,14 @@ def get_suburb_crime_and_weather():
 
 def compare_sentiment_and_crime():
     year = request.headers.get("X-Fission-Params-Year")
-    
+
     if not year:
         return make_response(jsonify({"error": "Year parameter is required"}), 400)
-    
+
     try:
         year = int(year)
     except ValueError:
         return make_response(jsonify({"error": "Year parameter must be an integer"}), 400)
-    
+
     result = SentimentService.compare_sentiment_and_crime(year)
     return make_response(jsonify(result), 200)
